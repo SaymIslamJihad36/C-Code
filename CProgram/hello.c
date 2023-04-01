59e2a360381dcc979a1639d018135b176a86c597
unknown <githubforvs@gmail.com>
2023-04-01 02:00:41 -0700

    setup

---
+++ CProgram/hello.c
@@ -0,0 +1,11 @@
+//                                   </>
+//                             SAYM ISLAM JIHAD
+//                      STUDENT AT PHITRON SPRING 2023
+//                           FB : SAYM ISLAM JIHAD
+//                                   </>
+
+
+#include<stdio.h>
+int main(void){
+    printf("Hello");
+}
