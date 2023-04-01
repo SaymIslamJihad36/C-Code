741418cedeef82668df6906beeb1b1d3b2b74492
unknown <githubforvs@gmail.com>
2023-04-01 11:01:39 -0700

    variable.c

---
+++ CProgram/variable.c
@@ -0,0 +1,17 @@
+//                                   </>
+//                             SAYM ISLAM JIHAD
+//                      STUDENT AT PHITRON SPRING 2023
+//                           FB : SAYM ISLAM JIHAD
+//                                   </>
+
+#include<stdio.h>
+
+int main()
+{
+    int saym = 10;
+    //saym = 10;
+    char any = 'F';
+    float an = 10.5;
+    printf("%d\n%c\n%0.2f",saym,any,an);
+    return 0;
+}
