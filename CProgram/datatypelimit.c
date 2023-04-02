9fbccd0f20e7c2ec4a05276e0a8c1112ac25baf5
unknown <githubforvs@gmail.com>
2023-04-01 20:25:53 -0700

    datatypelimit.c

---
+++ CProgram/datatypelimit.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    //int a = 10000000000;
+    int a = 1000000000;
+    long long int b = 1000000000;
+    double c = 2.567898879574657;
+    printf("%0.15lf\n",c);
+}
+
+// Rules of naming variable 
+/*
+        1) saym \right
+        2) _saym \right
+        3) @saym *wrong
+        4) 123saym *wrong
+*/
