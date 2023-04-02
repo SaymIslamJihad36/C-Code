2741df14e35c22267bbee2aebfc922968001b790
unknown <githubforvs@gmail.com>
2023-04-01 20:42:28 -0700

    datatypelimit.c

--- CProgram/datatypelimit.c
+++ CProgram/datatypelimit.c
@@ -15,19 +15,3 @@ int main()
     printf("%0.15lf\n",c);
 }
 
-// Rules of naming variable 
-/*
-        1) saym \right
-        2) _saym \right
-        3) @saym *wrong
-        4) 123saym *wrong
-        5) saym123 \right
-        6) saym_123 \right
-        7) saym 123 *wrong
-        8) saym,jihad *wrong
-        9) 
-
-///////2) variable name must be contain letter,digits or underscore
-///////3) keywords can't be used as variables
-            for,int,float
-*/
