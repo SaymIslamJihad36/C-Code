ccf0e899cc073bd8db4c47f16ab6d3846a1013e7
unknown <githubforvs@gmail.com>
2023-04-02 09:28:34 -0700

    hello.c

---
+++ CProgram/inout.c
@@ -0,0 +1,19 @@
+//                                   </>
+//                             SAYM ISLAM JIHAD
+//                      STUDENT AT PHITRON SPRING 2023
+//                           FB : SAYM ISLAM JIHAD
+//                                   </>
+
+
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    char p;
+    printf("Enter anything: ");
+    //scanf("%d%c %d%c",&a,&p,&b,&p);
+    //printf("%d%% %d%%\n",a,b);
+    scanf("%d%% %d%%",&a,&b,&p);
+    printf("%d%% %d%%",a,b);
+}
