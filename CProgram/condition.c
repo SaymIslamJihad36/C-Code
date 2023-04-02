c1b08692a1c65270cb671f9e358fa8368b81f191
unknown <githubforvs@gmail.com>
2023-04-02 11:06:09 -0700

    condition.c

---
+++ CProgram/condition.c
@@ -0,0 +1,34 @@
+//                                   </>
+//                             SAYM ISLAM JIHAD
+//                      STUDENT AT PHITRON SPRING 2023
+//                           FB : SAYM ISLAM JIHAD
+//                                   </>
+
+/*
+#include<stdio.h>
+
+int main(){
+    int tk;
+    scanf("%d",&tk);
+    if(tk==100){
+        printf("Burger khabo!");
+    }else{
+        printf("Saradin kichu khabo na!");
+    }
+
+    return 0;
+}*/
+
+#include<stdio.h>
+
+int main(){
+    int tk;
+    scanf("%d",&tk);
+    if(tk >= 100){
+        printf("Burger Khabo");
+    }else if(tk>=50){
+        printf("fucka khabo!");
+    }else{
+        printf("khabo na!");
+    }
+}
