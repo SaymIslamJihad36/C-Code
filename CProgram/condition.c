49e9b6251e75a9b755500179c29fd2dc0a797ad1
unknown <githubforvs@gmail.com>
2023-04-02 20:03:47 -0700

    condition.c

--- CProgram/condition.c
+++ CProgram/condition.c
@@ -17,7 +17,7 @@ int main(){
     }
 
     return 0;
-}*/
+}
 
 #include<stdio.h>
 
@@ -32,3 +32,26 @@ int main(){
         printf("khabo na!");
     }
 }
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int tk;
+    printf("Enter your money amount : ");
+    scanf("%d",&tk);
+    if(tk>=5000){
+        printf("Cox's a jbo\n");
+        if(tk>=10000){
+            printf("Saint Martin a jabo\n");
+        }else{
+            printf("Ferot cole jabo!\n");
+        }
+    }else{
+        printf("jawa hobe na tk kom!\n");
+    }
+    return 0;
+}
+
+
